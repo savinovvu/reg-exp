eeeebe148e24d8802e9f86bcfb3f9eb0545b1fad
skorpion <savinov_vu@inbox.ru>
2018-04-29 01:32:55 +0800

    set gradle and angular cli to build.

--- reg-exp/src/main/java/ru/inbox/savinov_vu/regexp/Main.java
+++ reg-exp/src/main/java/ru/inbox/savinov_vu/regexp/Main.java
@@ -1,9 +1,7 @@
 package ru.inbox.savinov_vu.regexp;
 
-import ru.inbox.savinov_vu.service.RegExpTaskService;
-
 public class Main {
     public static void main(String[] args) {
-        System.out.println(RegExpTaskService.checkRegExp("name", "name"));
+//        System.out.println(RegExpTaskService.checkRegExp("name", "name"));
     }
 }
--- reg-exp/src/main/java/ru/inbox/savinov_vu/regexp/RegExpApplication.java
+++ reg-exp/src/main/java/ru/inbox/savinov_vu/regexp/RegExpApplication.java
@@ -1,18 +1,23 @@
 package ru.inbox.savinov_vu.regexp;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
 @SpringBootApplication
-public class RegExpApplication implements CommandLineRunner {
+@EnableAutoConfiguration(exclude = {
+        SecurityAutoConfiguration.class
+})
+//public class RegExpApplication implements CommandLineRunner {
+public class RegExpApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(RegExpApplication.class, args);
     }
 
-    @Override
+ /*   @Override
     public void run(String... args) throws Exception {
         System.out.println("sldkfjsldfj");
-    }
+    }*/
 }
