3a377af997f2098d9bbe676d7de78165c4946e75
skorpion <savinov_vu@inbox.ru>
2018-05-01 14:55:40 +0800

    add prototype front-end project structure.

---
+++ reg-exp/front-end/src/app/components/level/level.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'reg-level',
+  templateUrl: './level.component.html',
+  styleUrls: ['./level.component.css']
+})
+export class LevelComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
