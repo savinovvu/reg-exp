a09b73d8055354f33fe388c9c75262e1b632aa74
skorpion <savinov_vu@inbox.ru>
2018-05-04 20:44:26 +0800

    update angular version.

--- reg-exp/front-end/src/app/app.module.ts
+++ reg-exp/front-end/src/app/app.module.ts
@@ -1,23 +1,23 @@
-import {BrowserModule} from '@angular/platform-browser';
-import {NgModule} from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
 
-import {AppComponent} from './app.component';
-import {MaterialModule} from "./material/material.module";
-import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
-import {NavigationComponent} from './components/navigation/navigation.component';
-import {RootRouter} from "./app.routes";
-import {HomeComponent} from './components/home/home.component';
-import {NewsComponent} from './components/news/news.component';
-import {ContactComponent} from './components/contact/contact.component';
-import {AboutComponent} from './components/about/about.component';
-import {TaskComponent} from './components/task/task.component';
-import {CourseComponent} from './components/course/course.component';
-import {LevelComponent} from './components/level/level.component';
-import {RestDataSourceService} from "./services/rest/rest-data-source.service";
-import {HttpClientModule} from "@angular/common/http";
-import {FormsModule} from "@angular/forms";
-import {LoginComponent} from './components/login/login.component';
+import { AppComponent } from './app.component';
+import { MaterialModule } from "./material/material.module";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { NavigationComponent } from './components/navigation/navigation.component';
+import { RootRouter } from "./app.routes";
+import { HomeComponent } from './components/home/home.component';
+import { NewsComponent } from './components/news/news.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { AboutComponent } from './components/about/about.component';
+import { TaskComponent } from './components/task/task.component';
+import { CourseComponent } from './components/course/course.component';
+import { LevelComponent } from './components/level/level.component';
+import { RestDataSourceService } from "./services/rest/rest-data-source.service";
+import { HttpClientModule } from "@angular/common/http";
+import { FormsModule } from "@angular/forms";
+import { LoginComponent } from './components/login/login.component';
 
 
 @NgModule({
@@ -31,7 +31,7 @@ import {LoginComponent} from './components/login/login.component';
     TaskComponent,
     CourseComponent,
     LevelComponent,
-    LoginComponent
+    LoginComponent,
   ],
   imports: [
     BrowserModule,
@@ -44,7 +44,7 @@ import {LoginComponent} from './components/login/login.component';
   providers: [
     RestDataSourceService
   ],
-  bootstrap: [AppComponent]
+  bootstrap: [ AppComponent ]
 })
 export class AppModule {
 }
