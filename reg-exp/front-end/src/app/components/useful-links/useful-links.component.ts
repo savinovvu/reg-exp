628060b811d3d2a714537553752af1c28a2f4cdd
skorpion <savinov_vu@inbox.ru>
2018-05-07 22:07:32 +0800

    add addTaskComponent and UsefulLinksComponent.

---
+++ reg-exp/front-end/src/app/components/useful-links/useful-links.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'reg-useful-links',
+  templateUrl: './useful-links.component.html',
+  styleUrls: ['./useful-links.component.css']
+})
+export class UsefulLinksComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
