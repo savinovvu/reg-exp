d3195bfb0582fff78ee3eef1a35fa6e042e1cd0d
skorpion <savinov_vu@inbox.ru>
2018-09-16 23:12:35 +0800

    23. fix align. add "@angular/flex-layout"

--- reg-exp/front-end/src/app/app.module.ts
+++ reg-exp/front-end/src/app/app.module.ts
@@ -2,10 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 
-import { MaterialModule } from "./material/material.module";
-import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { MaterialModule } from './material/material.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavigationComponent } from './components/common/navigation/navigation.component';
-import { RootRouter } from "./app.routes";
+import { RootRouter } from './app.routes';
 import { HomeComponent } from './components/projectInfo/home/home.component';
 import { NewsComponent } from './components/projectInfo/news/news.component';
 import { ContactComponent } from './components/projectInfo/contact/contact.component';
@@ -13,9 +13,9 @@ import { AboutComponent } from './components/projectInfo/about/about.component';
 import { TaskComponent } from './components/courseMain/task/task.component';
 import { CourseComponent } from './components/courseMain/course/course.component';
 import { LevelComponent } from './components/courseMain/level/level.component';
-import { RestDataSourceService } from "./services/rest/rest-data-source.service";
-import { HttpClientModule } from "@angular/common/http";
-import { FormsModule } from "@angular/forms";
+import { RestDataSourceService } from './services/rest/rest-data-source.service';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 import { LoginComponent } from './components/users/login/login.component';
 import { AddTaskComponent } from './components/courseMain/add-task/add-task.component';
 import { CheckWrongStatusPipe } from './pipes/check-status/check-wrong-status.pipe';
@@ -23,11 +23,12 @@ import { UsefulLinksComponent } from './components/courseMain/useful-links/usefu
 import { JoinPipe } from './pipes/join/join.pipe';
 import { RegexpCheckResultComponent } from './components/courseMain/regexp-check-result/regexp-check-result.component';
 import { ErrorComponent } from './components/common/error/error.component';
-import { ErrorService } from "./components/common/error/errorService/error.service";
+import { ErrorService } from './components/common/error/errorService/error.service';
 import { AddUserComponent } from './components/users/add-user/add-user.component';
-import { AppComponent } from "./appComponent/app.component";
-import { AuthGuard } from "./services/guards/auth.guard";
-import { UserService } from "./services/security/user.service";
+import { AppComponent } from './appComponent/app.component';
+import { AuthGuard } from './services/guards/auth.guard';
+import { UserService } from './services/security/user.service';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 
 
@@ -57,7 +58,8 @@ import { UserService } from "./services/security/user.service";
     BrowserAnimationsModule,
     RootRouter,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    FlexLayoutModule
   ],
   providers: [
     RestDataSourceService, ErrorService, AuthGuard, UserService
