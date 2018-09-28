a8a563760d32c44dacd4edf55481da193201915d
skorpion <savinov_vu@inbox.ru>
2018-09-29 01:01:25 +0800

    25. Actual sql scripts.

--- reg-exp/scripts.sql
+++ reg-exp/scripts.sql
@@ -2,13 +2,8 @@ INSERT INTO "public"."user" ("id", "name", "email", "login", "password", "enable
 INSERT INTO "public"."user" ("id", "name", "email", "login", "password", "enabled") VALUES (2, 'user2', 'tmp2@mail.ru' , 'login2','pass', true);
 INSERT INTO "public"."user" ("id", "name", "email", "login", "password", "enabled") VALUES (3, 'admin1',  'tmp3@mail.ru' , 'login3','pass', true);
 
-INSERT INTO "public"."authority" ("id", "name") VALUES (1, 'User');
-INSERT INTO "public"."authority" ("id", "name") VALUES (2, 'Admin');
-
-INSERT INTO "public"."user_authority" ("user_id", "role_id") VALUES (1, 1);
-INSERT INTO "public"."user_authority" ("user_id", "role_id") VALUES (2, 1);
-INSERT INTO "public"."user_authority" ("user_id", "role_id") VALUES (3, 2);
-
+INSERT INTO "public"."user_authorities" ("user_id", "authorities") VALUES (1, 'User');
+INSERT INTO "public"."user_authorities" ("user_id", "authorities") VALUES (2, 'Admin');
 
 INSERT INTO "public"."reg_exp_level" ("id", number, "description") VALUES (1, 1, 'первый уровень');
 INSERT INTO "public"."reg_exp_level" ("id", number, "description") VALUES (2, 2, 'второй уровень');
@@ -27,7 +22,6 @@ INSERT INTO "public"."reg_exp_task" ("id", "number", "enabled", "name", "descrip
 INSERT INTO "public"."reg_exp_task_answer" ("id", "answer", "reg_exp_task_id") VALUES (1, 'sss', 1);
 INSERT INTO "public"."reg_exp_task_answer" ("id", "answer", "reg_exp_task_id") VALUES (2, 'sssa', 1);
 INSERT INTO "public"."reg_exp_task_answer" ("id", "answer", "reg_exp_task_id") VALUES (3, 'sss', 2);
-
 
 INSERT INTO "public"."user_solvedregexplevel" ("user_id", "solvedregexplevel_id") VALUES (1, 1);
 INSERT INTO "public"."user_solvedregexplevel" ("user_id", "solvedregexplevel_id") VALUES (2, 1);
@@ -55,9 +49,12 @@ INSERT INTO "public"."like" ("id", "regexptask_id", "user_id") VALUES (2, 1, 1);
 INSERT INTO "public"."like" ("id", "regexptask_id", "user_id") VALUES (3, 2, 1);
 INSERT INTO "public"."like" ("id", "regexptask_id", "user_id") VALUES (4, 2, 3);
 
-INSERT INTO "public"."reg_exp_task_excluded_strings" ("regexptask_id", "excluded_strings") VALUES (1, 'exclude1');
-INSERT INTO "public"."reg_exp_task_excluded_strings" ("regexptask_id", "excluded_strings") VALUES (2, 'exclude2');
-INSERT INTO "public"."reg_exp_task_excluded_strings" ("regexptask_id", "excluded_strings") VALUES (3, 'exclude3');
+INSERT INTO "public"."reg_exp_task_excluded_strings" ("regexptask_id", "excluded_strings")
+VALUES (1, 'exclude1');
+INSERT INTO "public"."reg_exp_task_excluded_strings" ("regexptask_id", "excluded_strings")
+VALUES (2, 'exclude2');
+INSERT INTO "public"."reg_exp_task_excluded_strings" ("regexptask_id", "excluded_strings")
+VALUES (3, 'exclude3');
 
 INSERT INTO "public"."reg_exp_task_match_strings" ("regexptask_id", "match_strings") VALUES (1, 'match1');
 INSERT INTO "public"."reg_exp_task_match_strings" ("regexptask_id", "match_strings") VALUES (1, 'match1.2')
@@ -67,9 +64,12 @@ INSERT INTO "public"."reg_exp_task_match_strings" ("regexptask_id", "match_strin
 INSERT INTO "public"."reg_exp_task_match_strings" ("regexptask_id", "match_strings") VALUES (2, 'match2');
 INSERT INTO "public"."reg_exp_task_match_strings" ("regexptask_id", "match_strings") VALUES (3, 'match3');
 
-INSERT INTO "public"."reg_exp_task_required_sub_strings" ("regexptask_id", "required_sub_strings") VALUES (1, 'requiredstring1');
-INSERT INTO "public"."reg_exp_task_required_sub_strings" ("regexptask_id", "required_sub_strings") VALUES (2, 'requiredstring2');
-INSERT INTO "public"."reg_exp_task_required_sub_strings" ("regexptask_id", "required_sub_strings") VALUES (3, 'requiredstring3');
+INSERT INTO "public"."reg_exp_task_required_sub_strings" ("regexptask_id", "required_sub_strings")
+VALUES (1, 'requiredstring1');
+INSERT INTO "public"."reg_exp_task_required_sub_strings" ("regexptask_id", "required_sub_strings")
+VALUES (2, 'requiredstring2');
+INSERT INTO "public"."reg_exp_task_required_sub_strings" ("regexptask_id", "required_sub_strings")
+VALUES (3, 'requiredstring3');
 
 INSERT INTO "public"."reg_exp_task_excluded_answers" ("regexptask_id", "excluded_answers") VALUES (1, 'excluded1');
 INSERT INTO "public"."reg_exp_task_excluded_answers" ("regexptask_id", "excluded_answers") VALUES (2, 'excluded2');
