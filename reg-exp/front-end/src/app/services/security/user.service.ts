c0fcc9bdc73d4733a15f89b2b8529aef85965c43
skorpion <savinov_vu@inbox.ru>
2018-11-03 00:15:05 +0800

    31. implement save log-in with localstorage

--- reg-exp/front-end/src/app/services/security/user.service.ts
+++ reg-exp/front-end/src/app/services/security/user.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { isNil } from 'lodash';
 
 
 
@@ -7,15 +8,70 @@ import { Injectable } from '@angular/core';
 })
 export class UserService {
 
-  token: string;
+  private _token: string;
 
-  id: string;
+  private _id: string;
 
-  name: string;
+  private _name: string;
 
-  roles: string[];
+  private _roles: string[];
 
 
   constructor() {
+    const token = localStorage.getItem('reg-token');
+    this._token = isNil(token) ? null : token;
+
+    const id = localStorage.getItem('reg-id');
+    this._id = isNil(id) ? null : id;
+
+    const name = localStorage.getItem('reg-name');
+    this._name = isNil(name) ? null : name;
+
+    const roles = JSON.parse(localStorage.getItem('reg-roles'));
+    this._roles = isNil(roles) ? [] : roles;
+  }
+
+
+  get token(): string {
+    return this._token;
+  }
+
+
+  set token(value: string) {
+    localStorage.setItem('reg-token', JSON.stringify(value));
+    this._token = value;
+  }
+
+
+  get id(): string {
+    return this._id;
+  }
+
+
+  set id(value: string) {
+    localStorage.setItem('reg-id', JSON.stringify(value));
+    this._id = value;
+  }
+
+
+  get name(): string {
+    return this._name;
+  }
+
+
+  set name(value: string) {
+    localStorage.setItem('reg-name', JSON.stringify(value));
+    this._name = value;
+  }
+
+
+  get roles(): string[] {
+    return this._roles;
+  }
+
+
+  set roles(value: string[]) {
+    localStorage.setItem('reg-roles', JSON.stringify(value));
+    this._roles = value;
   }
 }
