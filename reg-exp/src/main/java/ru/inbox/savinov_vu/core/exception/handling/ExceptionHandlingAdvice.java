8e47a5e58db0d7a4d547e7a6ffede2201067486e
Savinov Vasiliy <savinov_vu@inbox.ru>
2020-04-18 08:34:05 +0800

    xx. small fixes

--- reg-exp/src/main/java/ru/inbox/savinov_vu/core/exception/handling/ExceptionHandlingAdvice.java
+++ reg-exp/src/main/java/ru/inbox/savinov_vu/core/exception/handling/ExceptionHandlingAdvice.java
@@ -10,9 +10,6 @@ import ru.inbox.savinov_vu.core.exception.AuthenticationException;
 import ru.inbox.savinov_vu.core.exception.flow.InvalidParameterException;
 import ru.inbox.savinov_vu.core.exception.flow.ValidationException;
 
-import java.io.PrintWriter;
-import java.io.StringWriter;
-
 
 
 @RestControllerAdvice
@@ -22,43 +19,37 @@ public class ExceptionHandlingAdvice {
 
   @ExceptionHandler
   public ResponseEntity handleDefault(Exception e) {
-    LOG.debug(getStackTrace(e));
-    return new ResponseEntity(getStackTrace(e), HttpStatus.INTERNAL_SERVER_ERROR);
+    LOG.debug(e.getMessage());
+    return new ResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR);
   }
 
 
   @ExceptionHandler(InvalidParameterException.class)
   public ResponseEntity handleDefault(InvalidParameterException e) {
-    LOG.debug(getStackTrace(e));
-    return new ResponseEntity(getStackTrace(e), HttpStatus.NOT_FOUND);
+    LOG.debug(e.getMessage());
+    return new ResponseEntity(HttpStatus.NOT_FOUND);
   }
 
 
   @ExceptionHandler(ValidationException.class)
   public ResponseEntity handleDefault(ValidationException e) {
-    LOG.debug(getStackTrace(e));
-    return new ResponseEntity(getStackTrace(e), HttpStatus.BAD_REQUEST);
+    LOG.debug(e.getMessage());
+    return new ResponseEntity(HttpStatus.BAD_REQUEST);
   }
 
 
   @ExceptionHandler(MethodArgumentNotValidException.class)
   public ResponseEntity handleDefault(MethodArgumentNotValidException e) {
-    LOG.debug(getStackTrace(e));
-    return new ResponseEntity(getStackTrace(e), HttpStatus.BAD_REQUEST);
+    LOG.debug(e.getMessage());
+    return new ResponseEntity(HttpStatus.BAD_REQUEST);
   }
 
 
   @ExceptionHandler(AuthenticationException.class)
   public ResponseEntity handleDefault(AuthenticationException e) {
-    LOG.debug(getStackTrace(e));
-    return new ResponseEntity(getStackTrace(e), HttpStatus.UNAUTHORIZED);
+    LOG.debug(e.getMessage());
+    return new ResponseEntity(HttpStatus.UNAUTHORIZED);
   }
 
 
-  private static String getStackTrace(final Throwable throwable) {
-    final StringWriter sw = new StringWriter();
-    final PrintWriter pw = new PrintWriter(sw, true);
-    throwable.printStackTrace(pw);
-    return sw.getBuffer().toString();
-  }
 }
